a9cc15d317c8b2911dabf5314475e8b002bb968f
Mortimerry <61872364+Mortimerry@users.noreply.github.com>
2024-06-13 13:54:00 +0200

    Create Task function

---
+++ client/src/CreateTask.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import './CreateTask.css';
+const CreateTask = ({ userId }) => {
+  const [description, setDescription] = useState('');
+  const [expPoints, setExpPoints] = useState(0);
+  const [difficulty, setDifficulty] = useState('');
+  const [deadline, setDeadline] = useState('');
+
+  const difficultyOptions = {
+    easy: 100,
+    medium: 200,
+    hard: 400,
+    extreme: 700,
+  };
+
+  const handleDifficultyChange = (e) => {
+    const selectedDifficulty = e.target.value;
+    setDifficulty(selectedDifficulty);
+    setExpPoints(difficultyOptions[selectedDifficulty]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:5000/api/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user_id: userId,
+        description,
+        exp_points: expPoints,
+        difficulty,
+        deadline,
+      }),
+    });
+    if (response.ok) {
+      alert('Misja dodana pomyślnie');
+    } else {
+      const errorData = await response.json();
+      alert(`Nie udało się dodać misji: ${errorData.message}`);
+    }
+  };
+
+  return (
+   <div className="create-task-container">
+      <h2>Dodaj Nową Misję</h2>
+    <form onSubmit={handleSubmit}>
+    <div className="form-group">
+        <label htmlFor="description">Opis Misji</label>
+      <textarea
+        type="text"
+        placeholder="Opis..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        required
+      />
+      </div>
+      <div className="form-group">
+      <label htmlFor="difficulty">Poziom trudności</label>
+      <select value={difficulty} onChange={handleDifficultyChange} required>
+        <option value="">Wybierz Poziom Trudności</option>
+        <option value="easy">Łatwy</option>
+        <option value="medium">Średni</option>
+        <option value="hard">Trudny</option>
+        <option value="extreme">Ekstremalny</option>
+        onChange={(e) => setDifficulty(e.target.value)}
+      </select>
+      </div>
+      <div className="form-group">
+      <label htmlFor="expPoints">Punkty doświadczenia</label>
+      <input
+        type="number"
+        placeholder="Experience Points"
+        value={expPoints}
+        onChange={(e) => setExpPoints(e.target.value)}
+        readOnly
+      />
+      </div>
+      <div className="form-group">
+      <label htmlFor="deadline">Termin wykonania</label>
+      <input
+        type="datetime-local"
+        placeholder="Deadline"
+        value={deadline}
+        onChange={(e) => setDeadline(e.target.value)}
+        required
+      />
+      </div>
+      <button type="submit">Create Task</button>
+    </form>
+   </div>
+  );
+};
+
+export default CreateTask;
